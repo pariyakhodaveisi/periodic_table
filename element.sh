0b1ca87b9fdcf7c443d6f65b52f79eeedc28995f
Pariya Khodaveisi <paria.khodaveissi@gmail.com>
2024-03-28 14:41:19 +0000

    feat: bring data

--- element.sh
+++ element.sh
@@ -14,9 +14,9 @@ if [[ $1 ]]
     then 
       echo "I could not find that element in the database."
   else
-    echo $ELEMENT | while IFS=\| read ATOMIC_NUMBER ATOMIC_MAS
+    echo $ELEMENT | while IFS=\| read ATOMIC_NUMBER ATOMIC_MASS MPC BPC SY NAME TYPE
     do
-      echo "$ATOMIC_NUMBER - $ATOMIC_MAS"
+      echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SY). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MPC celsius and a boiling point of $BPC celsius."
     done
 
   fi
