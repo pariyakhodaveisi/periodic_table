95ede4c3fd1374c448ee7e2da458bbe9f4669d6c
Pariya Khodaveisi <paria.khodaveissi@gmail.com>
2024-03-28 14:26:56 +0000

    feat: enable querying by atomic number or name

--- element.sh
+++ element.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table  --no-align --tuples-only -c"
 
+if [[ $1 ]]
+ then
+  if [[ ! $1 =~ ^[0-9]+$ ]]
+  then
+  ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number = $1 LIMIT 1")
+  else
+  ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.atomic_number=$1")
+  fi
+    if [[ -z $ELEMENT ]] 
+    then 
+      echo "I could not find that element in the database."
+  else
+    echo $ELEMENT | while read ATOMIC_NUMBER
+    do
+      echo $ATOMIC_NUMBER
+    done
+
+  fi
+else
+  echo "Please provide an element as an argument."
+fi
